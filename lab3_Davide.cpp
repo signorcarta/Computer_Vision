13079d406a237896389b5a1ab9883dc5973c9c7d
Signorcarta <46000153+signorcarta@users.noreply.github.com>
2019-04-15 17:20:43 +0200

    Update lab3_Davide.cpp

--- lab3_Davide.cpp
+++ lab3_Davide.cpp
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <iostream>
+#include <stdio.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
@@ -7,107 +9,122 @@
 using namespace std;
 using namespace cv;
 
+/// Function provided that shows the histograms________________________________________________________
+void showHistogram(std::vector<cv::Mat>& hists)
+{
+	// Min/Max computation
+	double hmax[3] = { 0,0,0 };
+	double min;
+	cv::minMaxLoc(hists[0], &min, &hmax[0]);
+	cv::minMaxLoc(hists[1], &min, &hmax[1]);
+	cv::minMaxLoc(hists[2], &min, &hmax[2]);
+
+	std::string wname[3] = { "blue", "green", "red" };
+	cv::Scalar colors[3] = { cv::Scalar(255,0,0), cv::Scalar(0,255,0),
+							 cv::Scalar(0,0,255) };
+
+	std::vector<cv::Mat> canvas(hists.size());
+
+	// Display each histogram in a canvas
+	for (int i = 0, end = hists.size(); i < end; i++)
+	{
+		canvas[i] = cv::Mat::ones(125, hists[0].rows, CV_8UC3);
+
+		for (int j = 0, rows = canvas[i].rows; j < hists[0].rows - 1; j++)
+		{
+			cv::line(
+				canvas[i],
+				cv::Point(j, rows),
+				cv::Point(j, rows - (hists[i].at<float>(j) * rows / hmax[i])),
+				hists.size() == 1 ? cv::Scalar(200, 200, 200) : colors[i],
+				1, 8, 0
+			);
+		}
+
+		cv::imshow(hists.size() == 1 ? "value" : wname[i], canvas[i]);
+		waitKey(0);
+	}
+}
+///____________________________________________________________________________________________________
+
 int main(int argc, char** argv) {
 
-	Mat src, dst;
+	//Parameters inizialization
+	Mat src;
+	Mat dst;
 	Mat b_hist, g_hist, r_hist;
 	Mat eq_b_hist, eq_g_hist, eq_r_hist;
-	vector<Mat> bgr_planes;
+
+	std::vector<Mat> bgr_planes;
+	std::vector<Mat> eq_bgr_planes;
+	std::vector<cv::Mat> histogram;
+	std::vector<cv::Mat> eq_histogram;
 	
 	int histSize = 256;
 	bool uniform = true; 
 	bool accumulate = false;
 
-	//Loading and displaying the image_______________________________________________________________
-	src = cv::imread("C:\\Users\\david\\source\\repos\\Histogram_equalization\\lena.png");
+	//Loading and displaying the image__________________________________________________________________
+	src = cv::imread("C:\\Users\\david\\source\\repos\\Histogram_equalization\\lena.png", 1);
 	cv::namedWindow("image", WINDOW_AUTOSIZE);
 	imshow("image", src);
 	waitKey(0);
-	//_______________________________________________________________________________________________
+	//__________________________________________________________________________________________________
 
-	//Calculating histograms_________________________________________________________________________
+	//Calculating histograms____________________________________________________________________________
 
 	/// Separate the image in 3 planes ( B, G and R )	
-	split(src, bgr_planes);
+	cv::split(src, bgr_planes);
 
 	/// Set the ranges (for B,G,R))
 	float range[] = { 0, 256 };
 	const float* histRange = { range };	
 
 	/// Compute the three histograms
-	calcHist(&bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate);
-	calcHist(&bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
-	calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
-
-	/// Draw the histograms for B, G and R
-	int hist_w = 512; int hist_h = 400;
-	int bin_w = cvRound((double)hist_w / histSize);
-
-	Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
-	Mat eq_histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
+	cv::calcHist(&bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate);
+	cv::calcHist(&bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
+	cv::calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
 	
-	/*
-	Normalize the result to [ 0, histImage.rows ], so that its values fall 
-	in the range indicated by the parameters entered.
-	*/
-	normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+	/// Properly filling the vector to be passed as parameter in the function below
+	histogram.push_back(b_hist);
+	histogram.push_back(g_hist);
+	histogram.push_back(r_hist);
 
-	/// Draw for each channel
-	for (int i = 1; i < histSize; i++)
-	{
-		line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(b_hist.at<float>(i - 1))),
-			Point(bin_w * (i), hist_h - cvRound(b_hist.at<float>(i))),
-			Scalar(255, 0, 0), 2, 8, 0);
-		line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(g_hist.at<float>(i - 1))),
-			Point(bin_w * (i), hist_h - cvRound(g_hist.at<float>(i))),
-			Scalar(0, 255, 0), 2, 8, 0);
-		line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(r_hist.at<float>(i - 1))),
-			Point(bin_w * (i), hist_h - cvRound(r_hist.at<float>(i))),
-			Scalar(0, 0, 255), 2, 8, 0);
-	}
-
-	/// Display
-	namedWindow("calcHist Demo", WINDOW_AUTOSIZE);
-	imshow("calcHist Demo", histImage);
-	waitKey(0);
-	//_______________________________________________________________________________________________
-
-	//Equalizing each channel________________________________________________________________________
-	equalizeHist(src, dst);
-
-	/// Separate the image in 3 planes ( B, G and R )	
-	split(dst, bgr_planes);
+	///Function call
+	showHistogram(histogram);
 		
-	/// Compute the three histograms
-	calcHist(&bgr_planes[0], 1, 0, Mat(), eq_b_hist, 1, &histSize, &histRange, uniform, accumulate);
-	calcHist(&bgr_planes[1], 1, 0, Mat(), eq_g_hist, 1, &histSize, &histRange, uniform, accumulate);
-	calcHist(&bgr_planes[2], 1, 0, Mat(), eq_r_hist, 1, &histSize, &histRange, uniform, accumulate);
+	//__________________________________________________________________________________________________
 
-	normalize(eq_b_hist, eq_b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	normalize(eq_g_hist, eq_g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	normalize(eq_r_hist, eq_r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+	//Equalizing each channel___________________________________________________________________________
 	
-	/// Draw for each channel
-	for (int i = 1; i < histSize; i++)
-	{
-		line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(eq_b_hist.at<float>(i - 1))),
-			Point(bin_w * (i), hist_h - cvRound(eq_b_hist.at<float>(i))),
-			Scalar(255, 0, 0), 2, 8, 0);
-		line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(eq_g_hist.at<float>(i - 1))),
-			Point(bin_w * (i), hist_h - cvRound(eq_g_hist.at<float>(i))),
-			Scalar(0, 255, 0), 2, 8, 0);
-		line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(r_hist.at<float>(i - 1))),
-			Point(bin_w * (i), hist_h - cvRound(eq_r_hist.at<float>(i))),
-			Scalar(0, 0, 255), 2, 8, 0);
-	}
+	cv::equalizeHist(b_hist, eq_b_hist);
+	cv::equalizeHist(g_hist, eq_g_hist);
+	cv::equalizeHist(r_hist, eq_r_hist);
+		
+	eq_histogram.push_back(eq_b_hist);
+	eq_histogram.push_back(eq_g_hist);
+	eq_histogram.push_back(eq_r_hist);
 
-	// Display histogrms of equalized channels and images
-	namedWindow("calcHist Demo", WINDOW_AUTOSIZE);
-	imshow("calcHist Demo", eq_histImage);
+	/// Dispplay equalized histogram
+	showHistogram(eq_histogram);
+
+	/// Display equalized image
+	merge(eq_histogram, dst);
+	cv::namedWindow("image", WINDOW_AUTOSIZE);
+	imshow("image", dst);
 	waitKey(0);
-	//_______________________________________________________________________________________________
-
+	
+	//__________________________________________________________________________________________________
+	
 	return 0;
 }
+
+//cvtColor(src, dst, COLOR_BGR2GRAY);
+	
+	//split(dst, eq_bgr_planes);
+	/*
+	calcHist(&eq_bgr_planes[0], 1, 0, Mat(), eq_b_hist, 1, &histSize, &histRange, uniform, accumulate);
+	calcHist(&eq_bgr_planes[1], 1, 0, Mat(), eq_g_hist, 1, &histSize, &histRange, uniform, accumulate);
+	calcHist(&eq_bgr_planes[2], 1, 0, Mat(), eq_r_hist, 1, &histSize, &histRange, uniform, accumulate);	
+	*/
+	
