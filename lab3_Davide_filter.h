ee8b3f44f372abb50357fe79f8163f0c65091fd0
Signorcarta <46000153+signorcarta@users.noreply.github.com>
2019-04-23 22:30:52 +0200

    Update lab3_Davide_filter.h

--- lab3_Davide_filter.h
+++ lab3_Davide_filter.h
@@ -1,6 +1,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <iostream>
 
 // Generic class implementing a filter with the input and output image data and the parameters
 class Filter {
@@ -9,112 +10,100 @@ class Filter {
 
 public:
 
-	// Constructor 
-	/// input_img: image to be filtered
-	/// filter_size : size of the kernel/window of the filter
+	// constructor 
+	// input_img: image to be filtered
+	// filter_size : size of the kernel/window of the filter
 	Filter(cv::Mat input_img, int filter_size);
 
-	/// perform filtering (in base class do nothing, to be reimplemented in the derived filters)
+	// perform filtering (in base class do nothing, to be reimplemented in the derived filters)
 	void doFilter();
 
-	/// get the output of the filter
+	// get the output of the filter
 	cv::Mat getResult();
 
-	/// set the window size (square window of dimensions size x size)
+	//set the window size (square window of dimensions size x size)
 	void setSize(int size);
 
-	/// get the Window Size
+	//get the Window Size
 	int getSize();
 
-	/// Data
+	String getType();
+
+	// Data
 
 protected:
 
-	/// input image
+	// input image
 	cv::Mat input_image;
 
-	/// output image (filter result)
+	// output image (filter result)
 	cv::Mat result_image;
 
-	/// window size
+	// window size
 	int filter_size;
 
-
+	String type;
 
 };
 
-// Medianian Filter________________________________________________________________________________
-class MedianFilter : public Filter {
-
-	/// place constructor
-	public : 
-		MedianFilter(cv::Mat src, int ksize, cv::Mat dst) :
-		Filter(input_image, filter_size) {
-		dst = result_image;
-		}
-
-		/// re-implement  doFilter()
-		void doFilter() {
-			cv::medianBlur(input_image, result_image, filter_size);
-	
-		}
-
-		// no additional parameters
-};
-//_________________________________________________________________________________________________
 
 
-//Gaussian Filter__________________________________________________________________________________
+///Gaussian Filter________________________________________________________
 class GaussianFilter : public Filter {
 
-	/// place constructor
-	public:
-		GaussianFilter(cv::Mat src, int ksize, cv::Mat dst, double sigma_X, double sigma_Y) :
-			Filter(input_image, filter_size) {
-			dst = result_image;
-			sigma_X = sigmaX;
-			sigma_Y = sigmaY;
-		}
+	// place constructor
+	GaussianFilter(cv::Mat input_img, int filter_size, double sigma);
 
+	void doFilter();
 
-		/// re-implement  doFilter()
-		void doFilter() {
+	void setSigma(float value);
 
-			cv::GaussianBlur(input_image, result_image, size, sigmaX, sigmaY);
+	double getSigma();
 
-		}
+	// additional parameter: standard deviation (sigma)
+protected:
 
-		/// additional parameter: standard deviation (sigma)
-		double sigmaX;
-		double sigmaY;
-		cv::Size size;
+	double sigma;
 
 };
-//_________________________________________________________________________________________________
+///_______________________________________________________________________
 
 
-//Bilateral filter_________________________________________________________________________________
+///Median Filter__________________________________________________________
+class MedianFilter : public Filter {
+	
+	// place constructor
+	MedianFilter(cv::Mat src, int size);
+	
+	void doFilter();
+	
+};
+///_______________________________________________________________________
+
+
+///Bilateral Filter_______________________________________________________
 class BilateralFilter : public Filter {
 
-	/// place constructor
-	public:
-		BilateralFilter (cv::Mat src, int ksize, cv::Mat dst, double sigma_range, double sigma_space) :
-			Filter(input_image, filter_size) {
-			dst = result_image;
-			sigma_range = sigmaRange;
-			sigma_space = sigmaSpace;
-		}
-
-	/// re-implement  doFilter()
-	void doFilter() {
-
-		cv::bilateralFilter(input_image, result_image, filter_size, sigmaRange, sigmaSpace);
-
-	}
 	
-	/// additional parameters: sigma_range, sigma_space
-	double sigmaRange;
-	double sigmaSpace;
+public:
+	// place constructor
+	BilateralFilter(cv::Mat src, int size, double sigmaColor, double sigmaSpace);
 
+	void doFilter();
+
+	void setSigmaColor(float value);
+
+	void setSigmaSpace(float value);
+
+	double getSigmaColor();
+
+	double getSigmaSpace();
+
+protected:
+
+	// additional parameters: sigma_color, sigma_space
+	double sigma_color;
+	double sigma_space;
+	   
 };
-
+///_______________________________________________________________________
