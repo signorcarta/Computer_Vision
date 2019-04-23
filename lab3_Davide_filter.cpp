5c4c0b3a20a707c150eec35123035488820ae57c
Signorcarta <46000153+signorcarta@users.noreply.github.com>
2019-04-23 22:30:13 +0200

    Update lab3_Davide_filter.cpp

--- lab3_Davide_filter.cpp
+++ lab3_Davide_filter.cpp
@@ -3,7 +3,7 @@
 #include <opencv2/imgproc.hpp>
 #include "filter.h"
 
-//using namespace cv;
+using namespace cv;
 
 // constructor
 Filter::Filter(cv::Mat input_img, int size) {
@@ -14,10 +14,9 @@ Filter::Filter(cv::Mat input_img, int size) {
 	filter_size = size;
 }
 
-// for base class do nothing (in derived classes it performs the corresponding filter)
-void Filter::doFilter() {
 
-	// it just returns a copy of the input image
+void Filter::doFilter() {
+	
 	result_image = input_image.clone();
 
 }
@@ -43,5 +42,90 @@ int Filter::getSize() {
 }
 
 
+//Gaussian___________________________________________________________________________________________________
+GaussianFilter::GaussianFilter(cv::Mat input_img, int size, double sigma) : Filter(input_img, size) {
 
-// Write your code to implement the Gaussian, median and bilateral filters
+	sigma = sigma;
+	type = "Gaussian";
+}
+	
+void GaussianFilter::doFilter() {
+
+	result_image = input_image.clone();
+	cv::GaussianBlur(input_image, result_image, cv::Size(filter_size, filter_size), sigma, 0, cv::BORDER_DEFAULT);
+	
+}
+
+void GaussianFilter::setSigma(float value) {
+
+	sigma = value;
+
+}
+
+double GaussianFilter::getSigma() {
+
+	return sigma;
+
+}
+
+//___________________________________________________________________________________________________________
+
+
+//Median_____________________________________________________________________________________________________
+MedianFilter::MedianFilter(cv::Mat input_img, int filter_size) : Filter(input_img, filter_size) {
+
+	type = "Median";
+
+}
+
+void MedianFilter::doFilter() {
+
+	result_image = input_image.clone();
+	cv::medianBlur(input_image, result_image, filter_size);
+
+}
+
+//___________________________________________________________________________________________________________
+
+
+//Bilateral__________________________________________________________________________________________________
+BilateralFilter::BilateralFilter(cv::Mat input_img, int filter_size, double sigmaColor, double sigmaSpace) : Filter(input_img, filter_size) {
+
+	sigmaColor = sigma_color;
+	sigmaSpace = sigmaSpace;
+	type = "Bilateral";
+
+}
+
+void BilateralFilter::doFilter() {
+
+	result_image = input_image.clone();
+	cv::bilateralFilter(input_image, result_image, 7, sigma_color, sigma_space, cv::BORDER_DEFAULT);
+
+}
+
+void BilateralFilter::setSigmaColor(float value) {
+
+	sigma_color = value;
+
+}
+
+void BilateralFilter::setSigmaSpace(float value) {
+
+	sigma_space = value;
+
+}
+
+double BilateralFilter::getSigmaColor() {
+
+	return sigma_color;
+}
+
+double BilateralFilter::getSigmaSpace() {
+
+	return sigma_space;
+}
+
+//___________________________________________________________________________________________________________
+		
+
