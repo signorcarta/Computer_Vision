4c78ac08457aa5871c95d4be923f109e5cb616e1
Signorcarta <46000153+signorcarta@users.noreply.github.com>
2019-05-30 11:20:17 +0200

    Add files via upload

---
+++ PreProcessing.h
@@ -0,0 +1,14 @@
+#ifndef PREPROCESS_H
+#define PREPROCESS_H
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+void Preprocess(Mat &srcImage, Mat &tresholdImage);
+
+Mat maxContrast(Mat grayImage);
+
+#endif
