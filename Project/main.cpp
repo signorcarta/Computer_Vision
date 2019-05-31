78d08459d5e4e5b224e817b4f257c06090a9b9c3
Signorcarta <46000153+signorcarta@users.noreply.github.com>
2019-05-31 15:52:31 +0200

    Update main.cpp

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,23 +1,52 @@
 #include<opencv2/core/core.hpp>
 #include<opencv2/highgui/highgui.hpp>
 #include<opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <stdio.h>
 
 #include "PreProcessing.h"
+#include "PlateDetection.h"
+
+
+
+#define SHOW_STEPS /// Comment/uncomment this line to hide/show intermediate steps 
+//#define PREPROCESS /// If preprocess is disabled ==> change input image in detectPlate()
 
 using namespace cv;
+using namespace std;
 
 int main(int argc, char** argv) {
-	Mat src = imread("C:\\Users\\david\\source\\repos\\Licence_plate_reading\\image1.png");
-	namedWindow("Originale image");
-	imshow("Originale image", src);
+
+	//Loading image
+	Mat src = imread("C:\\Users\\david\\source\\repos\\License_plate_recognition\\7.jpg");
+	cout << "Showing original image . . . \n\n" << endl;
+	namedWindow("ORIGINAL IMAGE");
+	imshow("ORIGINAL IMAGE", src);
 	waitKey();
-	
+
+	//Preprocessing
+#ifdef PREPROCESS
 	Mat tresholded;
 	Preprocess(src, tresholded);
-	namedWindow("Tresholded Image");
-	imshow("Tresholded Image", tresholded);
+#ifdef SHOW_STEPS
+	cout << "Showing tresholded image . . . \n\n" << endl;
+	namedWindow("TRESHOLDED IMAGE");
+	imshow("TRESHOLDED IMAGE", tresholded);
 	waitKey();
-	
+#endif
+#endif
+
+	//Plate detection using cascade classifier
+	Mat detected;
+	string path = "C:\\Users\\david\\source\\repos\\License_plate_recognition\\classifier\\haarcascade_russian_plate_number.xml";
+	detectPlate(src, detected, path);
+#ifdef SHOW_STEPS
+	cout << "Showing detected plate . . . \n\n" << endl;
+	namedWindow("DETECTED PLATE");
+	imshow("DETECTED PLATE", detected);
+	waitKey();
+#endif
+
 
 	return 0;
 }
