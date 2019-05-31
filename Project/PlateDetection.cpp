ebf590fe5dc85c959e7a25aad75e0d1bb3779a55
Signorcarta <46000153+signorcarta@users.noreply.github.com>
2019-05-31 15:52:00 +0200

    Update PlateDetection.cpp

--- Project/PlateDetection.cpp
+++ Project/PlateDetection.cpp
@@ -9,11 +9,11 @@ void detectPlate(Mat& image, Mat& detected, String& path) {
 	CascadeClassifier plateClassifier; ///Initializes a cascade classifier
 	plateClassifier.load(path); ///Loads a pretrained model
 	
-	plateClassifier.detectMultiScale(image, plates, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(24, 24)); ///Detect plates in the image
-	
+	plateClassifier.detectMultiScale(image, plates, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(20, 20)); ///Detect plates in the image
+
 	for (size_t i = 0; i < plates.size(); i++) {
 		
-		rectangle(image, plates[i], Scalar(0, 0, 255), 3); ///Draws rectangles on the image
+		rectangle(image, plates[i], Scalar(0, 0, 255), 3, 8); ///Draws rectangles on the image
 	}
 
 	detected = image.clone();
